a72eac07f2630fc412db46154a2520aa6b0c12d1
Ryzen 3400G <Ryzen 3400G@DESKTOP-K4041SB>
2021-07-01 15:56:34 -0300

    Templates

---
+++ src/config/Conexion.java
@@ -0,0 +1,20 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    Connection con;
+    public Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_gestion", "root", "admin");
+        } catch (Exception e) {
+        }
+    }
+    
+    public Connection getConnection(){
+        return con;
+    }
+    
+}
